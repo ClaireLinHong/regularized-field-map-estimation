d7112f7939362438cfd9f6bb8a072accef195b2c
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-11-10 08:06:13 -0500

    Add missing pi to Hz calculation per #5

--- functions/compute_rmsd.m
+++ functions/compute_rmsd.m
@@ -7,7 +7,11 @@ function error = compute_rmsd(argsError, x, varargin)
 % argsError [n,3] cell: n methods, each w/ name, time, x, each of length neval
 % output:
 % error [n,1] cell: y coordinates of plots, each of length neval
-% 
+%
+% This routine computes RMSD over the entire image, not just within the mask.
+%
+% 2022-11-20 Jeff Fessler: add missing pi to Hz calculation
+
 n = size(argsError,1);
 error = cell(n,1);
 x = x(:);
@@ -24,7 +28,7 @@ for In = 1:n
     tc = argsError{In,2};
     nevalc = length(tc);
     xc = reshape(argsError{In,3},[],nevalc);
-    error{In} = vecnorm(xc-x,2,1)/2/sqrt(numel(x));
+    error{In} = vecnorm(xc-x,2,1)/2/pi/sqrt(numel(x));
     if arg.plot == 1
         hold on;
         h(In) = semilogy(tc(1),error{In}(1),'.-'); 
